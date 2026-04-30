ca149f49d8f57af98e23162851f47951af253d7c
KG367 <anchouskostya@gmail.com>
2026-04-30 11:07:07 +0300

    Практика 22

--- server.js
+++ server.js
@@ -1,80 +1,15 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const { integer } = require('check-types');
+const express = require("express");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Подключение к MongoDB
-mongoose.connect('mongodb://YourMongoAdmin:1234@172.23.143.48:27017/admin')
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Connection error:', err));
-
-app.use(express.json());
-
-const userSchema = new mongoose.Schema({
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true, unique: true },
-    age: { type: Number },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now }
+app.get("/", (req, res) => {
+  res.json({
+    message: `Response from backend server`,
+    port: PORT
+  });
 });
 
-userSchema.index({ id: 1 })
-
-const User = mongoose.model('User', userSchema);
-
-app.post('/users', async (req, res) => {
-    try {
-        const user = new User(req.body);
-        await user.save();
-        res.status(201).send(user);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
-});
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.send(users);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-});
-
-app.get('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findById( req.params.id )
-        if (!user) return res.status(404).send('User not found');
-        res.send(user);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
-})
-
-app.patch('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true } // Возвращает обновленный документ
-        );
-        if (!user) return res.status(404).send('User not found');
-        res.send(user);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
-});
-
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) return res.status(404).send('User not found');
-        res.send(user);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started on port ${PORT}`);
 });
